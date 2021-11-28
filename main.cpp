9634f125059353065c0fdf2dd32a3263204990e4
Meruem117 <1572677319@qq.com>
2021-11-28 20:00:08 +0800

    update main

--- main.cpp
+++ main.cpp
@@ -5,15 +5,17 @@
 using namespace std;
 
 int main() {
+    int key = 5;
     SeqList seqList{};
     SeqList2 seqList2{};
 
     initDefaultSeqList(seqList);
     printSeqList(seqList);
-    cout << seqSearch(seqList, 1) << endl;
-
     initDefaultSeqList2(seqList2);
     printSeqList2(seqList2);
-    cout << seqSearchWithSentry(seqList2, 1) << endl;
+
+    cout << seqSearch(seqList, key) << endl;
+    cout << binSearch(seqList, key) << endl;
+    cout << seqSearchWithSentry(seqList2, key) << endl;
     return 0;
 }
